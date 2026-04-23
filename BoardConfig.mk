955e566d1377c90093317ba0d25f51eab3030abc
NSR-XINE <nasironfire07@gmail.com>
2026-04-23 20:00:24 +0200

    lisa: set stock dpi

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -190,7 +190,7 @@ SOONG_CONFIG_ufsbsg += ufsframework
 SOONG_CONFIG_ufsbsg_ufsframework := bsg
 
 # Screen density
-TARGET_SCREEN_DENSITY := 400
+TARGET_SCREEN_DENSITY := 391
 
 # Security patch level
 VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
