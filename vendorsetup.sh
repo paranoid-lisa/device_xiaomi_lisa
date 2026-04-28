4406808d2f7629515da793bc2f06b8ecae9ab1e8
NSR ➹ <130887619+NSR-XINE@users.noreply.github.com>
2026-04-28 22:50:07 +0400

    lisa: vendorsetup: do not reclone device sources
    Ship on vendor_aospa https://github.com/paranoid-lisa/android_vendor_aospa/commit/b9c1f8a03fdb2093c3ddd15c8661f3ee8c957bae

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,28 +20,12 @@ clone_if_not_exist() {
 # Decode camera license
 base64 -d device/xiaomi/lisa/configs/camera/secret > device/xiaomi/lisa/configs/camera/st_license.lic
 
-# Kernel
-if [ -d "$KERNEL_PATH" ]; then
-    echo "✔ Skipping kernel (already exists)"
-else
-    echo "⬇ Cloning kernel (depth 1)"
-    git clone --depth 1 -b uvite \
-        https://github.com/paranoid-lisa/kernel_xiaomi_lahaina.git \
-        "$KERNEL_PATH"
-fi
-
 # KernelSU-Next (submodules)
 if [ -d "$KERNEL_PATH" ]; then
     cd "$KERNEL_PATH"
     git submodule update --init --recursive
     cd -
 fi
-
-# Vendor
-clone_if_not_exist "https://github.com/paranoid-lisa/vendor_xiaomi_lisa.git" "uvite" "vendor/xiaomi/lisa"
-
-# Firmware
-clone_if_not_exist "https://github.com/paranoid-lisa/vendor_xiaomi_firmware.git" "uvite" "vendor/xiaomi/firmware"
 
 # Hardware (Dolby)
 clone_if_not_exist "https://github.com/paranoid-lisa/hardware_dolby.git" "XiaomiDolby-aospa" "hardware/dolby"
