a2d272142ac547cb732c22ac8b32b76456fd609e
NSR-XINE <nasironfire07@gmail.com>
2026-04-28 15:00:36 +0400

    lisa: vendorsetup: sync KernelSU-Next

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+KERNEL_PATH="kernel/xiaomi/lahaina"
+
+
+
 base64 -d device/xiaomi/lisa/configs/camera/secret > device/xiaomi/lisa/configs/camera/st_license.lic
 
 # Kernel
-git clone -b uvite https://github.com/paranoid-lisa/kernel_xiaomi_lahaina.git kernel/xiaomi/lahaina
+git clone -b uvite https://github.com/paranoid-lisa/kernel_xiaomi_lahaina.git $KERNEL_PATH
+
+# KernelSU-Next
+cd $KERNEL_PATH &&
+git submodule update --init --recursive
+&&
+cd
 
 # Vendor
 git clone -b uvite https://github.com/paranoid-lisa/vendor_xiaomi_lisa.git vendor/xiaomi/lisa
