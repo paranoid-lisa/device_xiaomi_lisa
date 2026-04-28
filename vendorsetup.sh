680a32f72b2737b24e2eb84f32bed85342344b1f
NSR ➹ <130887619+NSR-XINE@users.noreply.github.com>
2026-04-28 19:06:53 +0400

    lisa: vendorsetup: Refactor cloning logic into reusable function

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,25 +1,40 @@
 #!/bin/bash
 
+set -e
+
 KERNEL_PATH="kernel/xiaomi/lahaina"
 
+clone_if_not_exist() {
+    local repo_url=$1
+    local branch=$2
+    local dest=$3
 
+    if [ -d "$dest" ]; then
+        echo "✔ Skipping $dest (already exists)"
+    else
+        echo "⬇ Cloning $dest"
+        git clone -b "$branch" "$repo_url" "$dest"
+    fi
+}
 
+# Decode camera license (always run)
 base64 -d device/xiaomi/lisa/configs/camera/secret > device/xiaomi/lisa/configs/camera/st_license.lic
 
 # Kernel
-git clone -b uvite https://github.com/paranoid-lisa/kernel_xiaomi_lahaina.git $KERNEL_PATH
+clone_if_not_exist https://github.com/paranoid-lisa/kernel_xiaomi_lahaina.git uvite $KERNEL_PATH
 
-# KernelSU-Next
-cd $KERNEL_PATH &&
-git submodule update --init --recursive
-&&
-cd
+# KernelSU-Next (submodules)
+if [ -d "$KERNEL_PATH" ]; then
+    cd $KERNEL_PATH
+    git submodule update --init --recursive
+    cd -
+fi
 
 # Vendor
-git clone -b uvite https://github.com/paranoid-lisa/vendor_xiaomi_lisa.git vendor/xiaomi/lisa
+clone_if_not_exist https://github.com/paranoid-lisa/vendor_xiaomi_lisa.git uvite vendor/xiaomi/lisa
 
 # Firmware
-git clone -b uvite https://github.com/paranoid-lisa/vendor_xiaomi_firmware.git vendor/xiaomi/firmware
+clone_if_not_exist https://github.com/paranoid-lisa/vendor_xiaomi_firmware.git uvite vendor/xiaomi/firmware
 
-# Hardware
-git clone -b XiaomiDolby-aospa https://github.com/paranoid-lisa/hardware_dolby.git hardware/dolby
+# Hardware (Dolby)
+clone_if_not_exist https://github.com/paranoid-lisa/hardware_dolby.git XiaomiDolby-aospa hardware/dolby
