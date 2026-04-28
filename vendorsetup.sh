12dfd0fd19599e752164a254b6ce8822355ce049
NSR-XINE <nasironfire07@gmail.com>
2026-04-28 14:44:11 +0400

    lisa: vendorsetup: clone hardware_dolby

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 base64 -d device/xiaomi/lisa/configs/camera/secret > device/xiaomi/lisa/configs/camera/st_license.lic
+
+# Hardware
+git clone -b XiaomiDolby-aospa https://github.com/paranoid-lisa/hardware_dolby.git hardware/dolby
