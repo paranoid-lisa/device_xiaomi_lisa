d967047a641df86b5c7627324beac53f6e0d5f3e
NSR ➹ <130887619+NSR-XINE@users.noreply.github.com>
2026-04-28 19:18:32 +0400

    lisa: vendorsetup: fixed bash logic error
    -bash: local: can only be used in a function -bash: [: !=: unary operator expected

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,9 +5,9 @@ set -e
 KERNEL_PATH="kernel/xiaomi/lahaina"
 
 clone_if_not_exist() {
-    local repo_url=$1
-    local branch=$2
-    local dest=$3
+    repo_url="$1"
+    branch="$2"
+    dest="$3"
 
     if [ -d "$dest" ]; then
         echo "✔ Skipping $dest (already exists)"
@@ -17,24 +17,31 @@ clone_if_not_exist() {
     fi
 }
 
-# Decode camera license (always run)
+# Decode camera license
 base64 -d device/xiaomi/lisa/configs/camera/secret > device/xiaomi/lisa/configs/camera/st_license.lic
 
 # Kernel
-clone_if_not_exist https://github.com/paranoid-lisa/kernel_xiaomi_lahaina.git uvite $KERNEL_PATH
+if [ -d "$KERNEL_PATH" ]; then
+    echo "✔ Skipping kernel (already exists)"
+else
+    echo "⬇ Cloning kernel (depth 1)"
+    git clone --depth 1 -b uvite \
+        https://github.com/paranoid-lisa/kernel_xiaomi_lahaina.git \
+        "$KERNEL_PATH"
+fi
 
 # KernelSU-Next (submodules)
 if [ -d "$KERNEL_PATH" ]; then
-    cd $KERNEL_PATH
+    cd "$KERNEL_PATH"
     git submodule update --init --recursive
     cd -
 fi
 
 # Vendor
-clone_if_not_exist https://github.com/paranoid-lisa/vendor_xiaomi_lisa.git uvite vendor/xiaomi/lisa
+clone_if_not_exist "https://github.com/paranoid-lisa/vendor_xiaomi_lisa.git" "uvite" "vendor/xiaomi/lisa"
 
 # Firmware
-clone_if_not_exist https://github.com/paranoid-lisa/vendor_xiaomi_firmware.git uvite vendor/xiaomi/firmware
+clone_if_not_exist "https://github.com/paranoid-lisa/vendor_xiaomi_firmware.git" "uvite" "vendor/xiaomi/firmware"
 
 # Hardware (Dolby)
-clone_if_not_exist https://github.com/paranoid-lisa/hardware_dolby.git XiaomiDolby-aospa hardware/dolby
+clone_if_not_exist "https://github.com/paranoid-lisa/hardware_dolby.git" "XiaomiDolby-aospa" "hardware/dolby"
