588050065aefd52cc4fa3cf0567a8bd109ccc9d2
NSR-XINE <nasironfire07@gmail.com>
2026-04-28 14:50:14 +0400

    lisa: vendorsetup: clone device sources
    *Kernel*
    *Vendor*
    *Firmware*

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,5 +2,14 @@
 
 base64 -d device/xiaomi/lisa/configs/camera/secret > device/xiaomi/lisa/configs/camera/st_license.lic
 
+# Kernel
+git clone -b uvite https://github.com/paranoid-lisa/kernel_xiaomi_lahaina.git kernel/xiaomi/lahaina
+
+# Vendor
+git clone -b uvite https://github.com/paranoid-lisa/vendor_xiaomi_lisa.git vendor/xiaomi/lisa
+
+# Firmware
+git clone -b uvite https://github.com/paranoid-lisa/vendor_xiaomi_firmware.git vendor/xiaomi/firmware
+
 # Hardware
 git clone -b XiaomiDolby-aospa https://github.com/paranoid-lisa/hardware_dolby.git hardware/dolby
